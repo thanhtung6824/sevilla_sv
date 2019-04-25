b1d6cc79a4691807bc1424c115cf166f9f15b2e9
thanhtung6824@gmail.com <thanhtung6824@gmail.com>
2019-04-25 19:12:16 +0700

    update

--- constants/products.js
+++ constants/products.js
@@ -1,5 +1,6 @@
 const products = [
     {
+        display: 'SV-001',
         name: 'Bếp điện từ cao cấp',
         code: 'SV-10T',
         price: '19.990.000',
@@ -8,6 +9,7 @@ const products = [
         typeOfSale: 'highlight',
     },
     {
+        display: 'SV-002',
         name: 'Bếp điện từ cao cấp',
         code: 'SV-189TS',
         price: '19.990.000',
@@ -16,6 +18,7 @@ const products = [
         typeOfSale: 'bestofsale',
     },
     {
+        display: 'SV-003',
         name: 'Bếp điện từ cao cấp',
         code: 'SV-237IC',
         price: '19.990.000',
@@ -24,6 +27,7 @@ const products = [
         typeOfSale: 'bestofsale',
     },
     {
+        display: 'SV-004',
         name: 'Bếp điện từ cao cấp',
         code: 'SV-289TS',
         price: '19.990.000',
@@ -32,6 +36,7 @@ const products = [
         typeOfSale: 'bestofsale',
     },
     {
+        display: 'SV-005',
         name: 'Bếp điện từ cao cấp',
         code: 'SV-M100T',
         price: '19.990.000',
@@ -40,6 +45,7 @@ const products = [
         typeOfSale: 'bestofsale',
     },
     {
+        display: 'SV-006',
         name: 'Bếp điện từ cao cấp',
         code: 'SV-M70S',
         price: '19.990.000',
@@ -48,6 +54,7 @@ const products = [
         typeOfSale: 'bestofsale',
     },
     {
+        display: 'SV-007',
         name: 'Bếp điện từ cao cấp',
         code: 'SV-M80S',
         price: '19.990.000',
@@ -56,6 +63,7 @@ const products = [
         typeOfSale: 'bestofsale',
     },
     {
+        display: 'SV-008',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-235T',
         price: '16.990.000',
@@ -64,6 +72,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-009',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-T60D',
         price: '16.990.000',
@@ -72,6 +81,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-010',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-T60S',
         price: '16.990.000',
@@ -80,6 +90,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-011',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-T75S',
         price: '16.990.000',
@@ -88,6 +99,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-012',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-73II',
         price: '16.990.000',
@@ -96,6 +108,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-013',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-83II',
         price: '16.990.000',
@@ -104,6 +117,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-014',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-93II',
         price: '16.990.000',
@@ -112,6 +126,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-015',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-135T',
         price: '16.990.000',
@@ -120,6 +135,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-016',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-135TS',
         price: '16.990.000',
@@ -128,6 +144,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-017',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-137IC',
         price: '16.990.000',
@@ -136,6 +153,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-018',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-137II',
         price: '16.990.000',
@@ -144,6 +162,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-019',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-189T',
         price: '16.990.000',
@@ -152,6 +171,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-020',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-202IH',
         price: '16.990.000',
@@ -160,6 +180,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-021',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-212IH',
         price: '16.990.000',
@@ -168,6 +189,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-022',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-237II',
         price: '16.990.000',
@@ -176,6 +198,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-023',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-289T',
         price: '16.990.000',
@@ -184,6 +207,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-024',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-H136T',
         price: '16.990.000',
@@ -192,6 +216,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-025',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-M99T',
         price: '16.990.000',
@@ -200,6 +225,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-026',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-T70D',
         price: '16.990.000',
@@ -208,6 +234,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-027',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-T88S',
         price: '16.990.000',
@@ -216,6 +243,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-028',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-T90S',
         price: '16.990.000',
@@ -224,6 +252,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-029',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-M100D',
         price: '16.990.000',
@@ -232,6 +261,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-030',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-M200T',
         price: '16.990.000',
@@ -240,6 +270,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-031',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-M300T',
         price: '16.990.000',
@@ -248,6 +279,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-032',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-M500T',
         price: '16.990.000',
@@ -256,6 +288,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-033',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-637IC',
         price: '16.990.000',
@@ -264,6 +297,7 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-034',
         name: 'Bếp điện từ cao cấp SEVILLA',
         code: 'SV-637II',
         price: '16.990.000',
@@ -272,348 +306,406 @@ const products = [
         typeOfSale: 'new'
     },
     {
+        display: 'SV-035',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-272S',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'bestofsale'
     },
     {
+        display: 'SV-036',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-702S',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'bestofsale'
     },
     {
+        display: 'SV-037',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-720S',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'bestofsale'
     },
     {
+        display: 'SV-038',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-728',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'bestofsale'
     },
     {
+        display: 'SV-039',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-818',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'bestofsale'
     },
     {
+        display: 'SV-040',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-101',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-041',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-201',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-042',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-209Black',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-043',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-209Caro',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-044',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-228Black',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-045',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-302',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-046',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-401',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-047',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-502',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-048',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-718',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-049',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-808',
         typeOfProduct: 'ga2lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-050',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-373S',
         typeOfProduct: 'ga3lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-051',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-703S',
         typeOfProduct: 'ga3lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-052',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-738',
         typeOfProduct: 'ga3lo',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-053',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-01GT',
         typeOfProduct: 'gahh',
         typeOfSale: 'new'
     },
     {
+        display: 'SV-054',
         name: 'Bếp gas cao cấp SEVILLA',
         code: 'SV-02GT',
         typeOfProduct: 'gahh',
         typeOfSale: 'new'
     },
     {
+        display: 'SV-055',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-7090TS',
         typeOfProduct: 'kmchut',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-056',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-TK90',
         typeOfProduct: 'kmchut',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-057',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-TS90',
         typeOfProduct: 'kmchut',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-058',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-08Black',
         typeOfProduct: 'kmcodien',
         typeOfSale: 'bestofsale'
     },
     {
+        display: 'SV-059',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-70W',
         typeOfProduct: 'kmcodien',
         typeOfSale: 'bestofsale'
     },
     {
+        display: 'SV-060',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-70WR',
         typeOfProduct: 'kmcodien',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-061',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-270Black',
         typeOfProduct: 'kmcodien',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-062',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-270Inox',
         typeOfProduct: 'kmcodien',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-063',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-700C',
         typeOfProduct: 'kmcodien',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-064',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-6070Black',
         typeOfProduct: 'kmcodien',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-065',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-6070Inox',
         typeOfProduct: 'kmcodien',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-066',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-600700SYP',
         typeOfProduct: 'kmcodien',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-067',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-R70B',
         typeOfProduct: 'kmcodien',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-068',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-R70I',
         typeOfProduct: 'kmcodien',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-069',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-900D1',
         typeOfProduct: 'kmdoclap',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-070',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-900D2',
         typeOfProduct: 'kmdoclap',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-071',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-DL290',
         typeOfProduct: 'kmdoclap',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-072',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-380',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'bestofsale'
     },
     {
+        display: 'SV-073',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-180190',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'bestofsale'
     },
     {
+        display: 'SV-074',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-170',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-075',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-3388S17090',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-076',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-470490',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-077',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-527529',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-078',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-570590',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-079',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-670690',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-080',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-870890',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-081',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-33887090',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-082',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-Lexus7090',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'highlight'
     },
     {
+        display: 'SV-083',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-320',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'new'
     },
     {
+        display: 'SV-084',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-325',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'new'
     },
     {
+        display: 'SV-085',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-327',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'new'
     },
     {
+        display: 'SV-086',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-350',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'new'
     },
     {
+        display: 'SV-087',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-370',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'new'
     },
     {
+        display: 'SV-088',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-386',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'new'
     },
     {
+        display: 'SV-089',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-H360',
         typeOfProduct: 'kmkinhcong',
         typeOfSale: 'new'
     },
     {
+        display: 'SV-090',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-90K2',
         typeOfProduct: 'kmkinhvat',
         typeOfSale: 'new'
     },
     {
+        display: 'SV-091',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-90K3',
         typeOfProduct: 'kmkinhvat',
         typeOfSale: 'new'
     },
     {
+        display: 'SV-092',
         name: 'Máy hút khử mùi cao cấp SEVILLA',
         code: 'SV-7090K1',
         typeOfProduct: 'kmkinhvat',
